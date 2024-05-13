c31be386c72ec322178db9e2aec157e609322784
Michael Lazear <michaellazear92@gmail.com>
2024-05-13 08:46:29 -0700

    update'

--- obj/Release/net7.0/ThermoParquet.AssemblyInfo.cs
+++ obj/Release/net7.0/ThermoParquet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThermoParquet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec80ae9695b540c2a15a96a29b6ec1d02706747e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53db0734ffb2841eba7f5c1f98ab24827267542b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThermoParquet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThermoParquet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
